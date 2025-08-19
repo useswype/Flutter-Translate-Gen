a5f55e4048f3419f75f828b77b96652366c4184d
Mostafa Soliman <mostafamoma.moma@gmail.com>
2025-08-19 17:46:48 +0300

    Chore: Refactor code for consistency and update dependencies

--- lib/keys_class_generator.dart
+++ lib/keys_class_generator.dart
@@ -2,29 +2,29 @@
 import 'package:flutter_translate_annotations/flutter_translate_annotations.dart';
 import 'package:flutter_translate_gen/localized_item.dart';
 
-class KeysClassGenerator
-{
-    static Reference get stringType => TypeReference((trb) => trb..symbol = "String");
+class KeysClassGenerator {
+  static Reference get stringType =>
+      TypeReference((trb) => trb..symbol = "String");
 
-    static Class generateClass(TranslateKeysOptions options, List<LocalizedItem> items, String className)
-    {
-        return Class((x) => x
-            ..name = className.substring(2)
-            ..fields.addAll(items
-                    .map((translation) => generateField(translation, options))
-                    .toList()),
-        );
-    }
+  static Class generateClass(TranslateKeysOptions options,
+      List<LocalizedItem> items, String className) {
+    return Class(
+      (x) => x
+        ..name = className.substring(2)
+        ..fields.addAll(items
+            .map((translation) => generateField(translation, options))
+            .toList()),
+    );
+  }
 
-    static Field generateField(LocalizedItem item, TranslateKeysOptions options)
-    {
-        return Field((x) => x
-                ..name = item.fieldName
-                ..type = stringType
-                ..static = true
-                ..modifier = FieldModifier.constant
-                ..assignment = literalString(item.key).code,
-        );
-    }
+  static Field generateField(LocalizedItem item, TranslateKeysOptions options) {
+    return Field(
+      (x) => x
+        ..name = item.fieldName
+        ..type = stringType
+        ..static = true
+        ..modifier = FieldModifier.constant
+        ..assignment = literalString(item.key).code,
+    );
+  }
 }
-
